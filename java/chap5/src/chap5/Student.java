37432772fba1c764f8efec5a458a45697fe7775a
kywu9232 <kywu9232@naver.com>
2022-07-01 23:31:28 +0900

    2022.07.01

--- java/chap5/src/chap5/Student.java
+++
@@ -1,8 +0,0 @@
-package chap5;
-
-public interface Student {
-	/*public final static 자동정의*/String name="학생";
-	/*abstract public 자동정의*/void study();
-	void lunch();
-
-}
--- java/chap5/src/chap5/Worker.java
+++
@@ -1,8 +0,0 @@
-package chap5;
-
-public interface Worker {
-	String name = "교직원";
-	void work();
-	void lunch();
-
-}
